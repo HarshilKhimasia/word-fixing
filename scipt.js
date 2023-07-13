e6aa7cf88a4b17d9afdea6e0b7359dfc08f31b0e
Harshil Khimasia <118171733+HarshilKhimasia@users.noreply.github.com>
2023-07-14 02:56:56 +0530

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,6 +1,5 @@
 let passenger = 'jOnAs';
 const passengerLower = passenger.toLowerCase();
-console.log(passengerLower);
 const firstLetter = passengerLower.charAt(0);
 const capitalFirstLetter = firstLetter.toUpperCase();
 passenger = `${capitalFirstLetter}${passengerLower.slice(1)}`;
